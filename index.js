e6df4b9b8ce80b9ccfe313a4b78d35619f5b8529
Marcato Luca <marcato@intre.it>
2023-08-11 12:50:21 +0200

    fix image extension

--- index.js
+++ index.js
@@ -5,44 +5,46 @@ const BASE_URL = `http://localhost:${port}`
 
 app.use(express.static('images'))
 
+const imagesName = [
+    'frame-06.jpg',
+    'frame-07.jpg',
+    'frame-08.jpg',
+    'frame-09.jpg',
+    'frame-10.jpg',
+    'frame-11.jpg',
+    'frame-12.jpg',
+    'frame-13.jpg',
+    'frame-14.jpg',
+    'frame-15.jpg',
+    'frame-16.jpg',
+    'frame-17.jpg',
+    'frame-18.jpg',
+    'frame-19.jpg',
+    'frame-20.jpg',
+    'frame-21.jpg',
+    'frame-22.jpg',
+    'frame-23.jpg',
+    'frame-24.jpg',
+    'frame-25.jpg',
+    'frame-26.jpg',
+    'frame-27.jpg',
+    'frame-28.jpg',
+    'frame-29.jpg',
+    'frame-30.jpg',
+    'frame-31.jpg',
+    'frame-32.jpg',
+    'frame-33.jpg',
+    'frame-34.jpg',
+    'frame-35.jpg',
+    'frame-36.jpg',
+    'frame-37.jpg',
+    'frame-38.jpg',
+    'frame-39.jpg',
+    'frame-40.jpg'
+];
+
 app.get('/images', (req, res) => {
-    res.json([
-        'frame-06.png',
-        'frame-07.png',
-        'frame-08.png',
-        'frame-09.png',
-        'frame-10.png',
-        'frame-11.png',
-        'frame-12.png',
-        'frame-13.png',
-        'frame-14.png',
-        'frame-15.png',
-        'frame-16.png',
-        'frame-17.png',
-        'frame-18.png',
-        'frame-19.png',
-        'frame-20.png',
-        'frame-21.png',
-        'frame-22.png',
-        'frame-23.png',
-        'frame-24.png',
-        'frame-25.png',
-        'frame-26.png',
-        'frame-27.png',
-        'frame-28.png',
-        'frame-29.png',
-        'frame-30.png',
-        'frame-31.png',
-        'frame-32.png',
-        'frame-33.png',
-        'frame-34.png',
-        'frame-35.png',
-        'frame-36.png',
-        'frame-37.png',
-        'frame-38.png',
-        'frame-39.png',
-        'frame-40.png'
-    ].map((image) => `${BASE_URL}/${image}`))
+    res.json(imagesName)
 })
 
 app.listen(port, () => {
